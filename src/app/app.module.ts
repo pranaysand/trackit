7f169b752ff14a6727fb4d2030481d4f26846c38
Pranay S <pranay.livingstone@gmail.com>
2018-02-05 01:43:49 -0800

    Created components ng serve

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
+import{RouterModule,Routes} from "@angular/router";
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
+import {NavbarComponent} from "./components/navbar/navbar.component";
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ContactsComponent } from './components/contacts/contacts.component';
+import { ContactDetailsComponent } from './components/contact-details/contact-details.component';
+
+//Creating Routes.
+const appRoutes: Routes=[
+  {path:'',component:DashboardComponent},
+  {path:'contacts',component:ContactsComponent},
+]
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    DashboardComponent,
+    ContactsComponent,
+    ContactDetailsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
