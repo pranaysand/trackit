822529b1a525bd1e4c0e8d64066f8d53da10c9d7
Pranay S <pranay.livingstone@gmail.com>
2018-02-06 02:14:34 -0800

    Added angularfire2 and firebase to the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,8 @@
 import{RouterModule,Routes} from "@angular/router";
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { AngularFireStorageModule } from 'angularfire2/storage';
+import { AngularFireAuthModule } from 'angularfire2/auth';
 
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
@@ -10,6 +14,8 @@ import { ContactDetailsComponent } from './components/contact-details/contact-de
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { ClientsComponent } from './components/clients/clients.component';
+
+import{environment} from '../environments/environment';
 
 //Creating Routes.
 const appRoutes: Routes=[
@@ -30,11 +36,16 @@ const appRoutes: Routes=[
     ContactDetailsComponent,
     LoginComponent,
     RegisterComponent,
-    ClientsComponent
+    ClientsComponent,
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    AngularFireModule.initializeApp(environment.firebase,"trackit"), // imports firebase/app needed for everything
+    AngularFirestoreModule, // imports firebase/firestore, only needed for database features
+    AngularFireAuthModule, // imports firebase/auth, only needed for auth features,
+    AngularFireStorageModule // imports firebase/storage only needed for storage features
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
