f27de26e62b3b997ec38c283f5c10760e0200047
Pranay S <pranay.livingstone@gmail.com>
2018-02-07 05:19:58 -0800

    Changes to take customers component to take effect.

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {CustomersService} from '../../services/customers.service';
+import{Customer} from "../../models/customer";
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +8,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+customers:Customer[];
+  constructor(private customersService:CustomersService) {
 
-  constructor() { }
+   }
 
   ngOnInit() {
-  }
+}
 
 }
